ee8614eaa07e92cf2b183e5f7d81fa06a94fe6f6
poface64 <angelgb064@gmail.com>
2025-04-10 21:43:59 -0600

    Avance abril 10

--- VISION POR COMPUTADORA/EJERCICIOS/CAPITULO 5/Code/sift_registration.m
+++ VISION POR COMPUTADORA/EJERCICIOS/CAPITULO 5/Code/sift_registration.m
@@ -1,17 +1,23 @@
-close all
-clear all
 
-% Use SURF Algorithm
-original  = imread('cameraman.tif');
+
+
+%% Use SURF Algorithm %%
+ruta = "C:\\Users\\Angel\\Desktop\\EJERCICIO BIOID\\BioID1520 1\\BioID1520\\Im_Faces\\BioID_0151.pgm"
+original=(imread(ruta)); %1515
+%original  = imread('cameraman.tif');
+subplot(2,3,1)
 imshow(original);
 title('Base image');
 
+%% Aplicando rotaciones %%
+
 distorted = imresize(original,0.7); 
 distorted = imrotate(distorted,31);
-figure; imshow(distorted);
+subplot(2,3,2)
+imshow(distorted);
 title('Transformed image');
 
-%Detect and extract features from both images.
+%% Detect and extract features from both images.
 
 ptsOriginal  = detectSURFFeatures(original);
 ptsDistorted = detectSURFFeatures(distorted);
@@ -20,31 +26,32 @@ ptsDistorted = detectSURFFeatures(distorted);
 [featuresDistorted,validPtsDistorted] = ...
     extractFeatures(distorted,ptsDistorted);
 
-%Match features.
-
 index_pairs = matchFeatures(featuresOriginal,featuresDistorted);
 matchedPtsOriginal  = validPtsOriginal(index_pairs(:,1));
 matchedPtsDistorted = validPtsDistorted(index_pairs(:,2));
-figure; 
+
+%% Hacer el match %% 
+subplot(2,3,3)
 showMatchedFeatures(original,distorted,...
     matchedPtsOriginal,matchedPtsDistorted);
 title('Matched SURF points,including outliers');
 
+%% Excluir los outliers %%
 %Exclude the outliers, and compute the transformation matrix.
 
 [tform,inlierPtsDistorted,inlierPtsOriginal] = ...
     estimateGeometricTransform(matchedPtsDistorted,matchedPtsOriginal,...
     'similarity');
-figure; 
-
+subplot(2,3,4)
 showMatchedFeatures(original,distorted,...
     inlierPtsOriginal,inlierPtsDistorted);
 title('Matched inlier points');
 
-%Recover the original image from the distorted image.
+%% Recover the original image from the distorted image.
 
 outputView = imref2d(size(original));
 Ir = imwarp(distorted,tform,'OutputView',outputView);
-figure; imshow(Ir); 
+subplot(2,3,5)
+imshow(Ir); 
 title('Recovered image');
 
